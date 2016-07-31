f3399c89ef33070b4ac3e9ffca05f61dcec8c1ef
Madz <madsgt@gmail.com>
2016-07-31 05:58:18 +0000

    Practice SQL complete

---
+++ more-practice.sql
@@ -0,0 +1,15 @@
+-- Include your solutions to the More Practice problems in this file.
+
+# solution to SQL[13] SELECT b.name, m.name, m.year, b.discontinued, (b.discontinued - m.year) AS years_until_brand_discontinued FROM models AS m LEFT JOIN brands AS b ON m.brand_name = b.name
+# WHERE b.discontinued IS NOT NULL ORDER BY b.name, m.name, m.year;
+
+
+
+
+-- INSERT
+
+
+-- CREATE TABLE
+
+
+-- More INSERT
