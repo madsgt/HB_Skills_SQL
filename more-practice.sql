caec4107f3bafc8c33b9fbc7e2c2079af91cb38d
Madz <madsgt@gmail.com>
2016-07-31 06:18:38 +0000

    More practice SQL complete

--- more-practice.sql
+++ more-practice.sql
@@ -1,6 +1,9 @@
 -- Include your solutions to the More Practice problems in this file.
 
-# solution to SQL[13] SELECT b.name, m.name, m.year, b.discontinued, (b.discontinued - m.year) AS years_until_brand_discontinued FROM models AS m LEFT JOIN brands AS b ON m.brand_name = b.name
+# solution to SQL[13] entering here as results were good match but still it wouldnt pass
+# SELECT b.name, m.name, m.year, b.discontinued, (b.discontinued - m.year) AS years_until_brand_discontinued 
+# FROM models AS m LEFT JOIN brands AS b 
+# ON m.brand_name = b.name
 # WHERE b.discontinued IS NOT NULL ORDER BY b.name, m.name, m.year;
 
 
@@ -8,8 +11,24 @@
 
 -- INSERT
 
+INSERT INTO models (year, brand_name, name) VALUES
+(2015, 'Chevrolet', 'Malibu'),
+(2015, 'Subaru', 'Outback');
+
 
 -- CREATE TABLE
 
+CREATE TABLE awards(
+id SERIAL PRIMARY KEY,
+name VARCHAR(50),
+year INTEGER,
+winner_id INTEGER NOT NULL
+);
+
 
 -- More INSERT
+
+INSERT INTO awards (name, year, winner_id) VALUES
+('IIHS Safety Award', 2015, 49),
+('IIHS Safety Award', 2015, 50);
+
